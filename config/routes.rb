6b13e835efd5df16345ddf83320237ea8e9cb261
fabianestebanmolina <fabian.makt@gmail.com>
2021-12-12 17:45:52 -0300

    carpetav1 yModelos-validacion de email-password-e-formato@validado

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Api defination
-  namespace :api, defaults: { format: :json} do    
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+
+    end
   end
 end
