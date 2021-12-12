100364ca4254e14eec59e4a9bd7c0315ba5c3576
fabianestebanmolina <fabian.makt@gmail.com>
2021-12-12 17:15:10 -0300

    Set versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Api defination
+  namespace :api, defaults: { format: :json} do    
+  end
 end
