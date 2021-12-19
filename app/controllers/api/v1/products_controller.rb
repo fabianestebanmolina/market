2e0aa2883657f6f2cd2169b435b38f0f3a16b158
fabianestebanmolina <fabian.makt@gmail.com>
2021-12-19 18:17:52 -0300

    Add user relationship to product serializerPT2

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::ProductsController < ApplicationController
   before_action :check_login, only: %i[create]
   before_action :check_owner, only: %i[update destroy]
   def show
-    render json: ProductSerializer.new(@product, url_options).serializable_hash
+    options = { include: [:user] }
+    render json: ProductSerializer.new(@product, options).serializable_hash
   end
 
   def index
-    render json: Product.all
     render json: ProductSerializer.new(@products).serializable_hash
   end
 
