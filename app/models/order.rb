3c166580d90a2443c7c460d8fbfed4bbcc34ca5b
fabianestebanmolina <fabian.makt@gmail.com>
2022-01-01 18:39:30 -0300

    Adds the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,12 @@
 class Order < ApplicationRecord
+  before_validation :set_total!
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   has_many :placements, dependent: :destroy # por que el test no pasa al invertir las posiciones de la L5 Y L6?????
   has_many :products, through: :placements
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
