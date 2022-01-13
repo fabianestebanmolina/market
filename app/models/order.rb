99033a9495e047cf4ef8dd10d2e4396bcea54dfa
fabianestebanmolina <fabian.makt@gmail.com>
2022-01-13 18:21:50 -0300

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,13 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
   before_validation :set_total!
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   has_many :placements, dependent: :destroy # por que el test no pasa al invertir las posiciones de la L5 Y L6?????
   has_many :products, through: :placements
+
+  validates_with EnoughProductsValidator
 
   def set_total!
     self.total = products.map(&:price).sum
